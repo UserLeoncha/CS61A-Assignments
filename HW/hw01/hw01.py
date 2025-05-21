3ca8070b755de801929c7252ebcfbc35a4e66218
UserLeoncha <iostream031118@gmail.com>
2025-05-21 17:51:38 +0800

    hw01 done

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,8 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+
+    return i*i+j*j+k*k-max(i,j,k)**2
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,7 +67,9 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    for max in range(n-1, 0, -1):
+        if n % max == 0:
+            return max
 
 def hailstone(n):
     """Print the hailstone sequence starting at n and return its
@@ -88,4 +91,13 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    res = 1
+    print(int(n))
+    while n != 1:
+        res=res+1
+        if n%2==0:
+            n = n/2
+        else:
+            n = 3*n+1
+        print(int(n))
+    return res
